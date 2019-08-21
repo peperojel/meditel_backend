2e17848298a8899f90d33bd9eb24f255a84b44b1
peperojel <joserojel9@gmail.com>
2019-08-21 20:12:32 -0300

    Database config changed

--- config/database.js
+++ config/database.js
@@ -59,7 +59,7 @@ module.exports = {
       user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
       database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
-    } 
+    },
     debug: Env.get('DB_DEBUG', false)
   },
 
