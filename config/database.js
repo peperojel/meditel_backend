033835c5009e23837fb61aed79d513d01943e136
peperojel <joserojel9@gmail.com>
2019-08-21 19:43:44 -0300

    Database config changed

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const Env = use('Env')
 const Helpers = use('Helpers')
 
 const Url = require('url-parse')
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+const CLEARDB_DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 module.exports = {
   /*
@@ -54,11 +54,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.hostname),
-      port: Env.get('DB_PORT', DATABASE_URL.port),
-      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', CLEARDB_DATABASE_URL.port),
+      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
     },
     debug: Env.get('DB_DEBUG', false)
   },
