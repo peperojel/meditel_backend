d6fa98dc1cdd1ad3ad03d095802ed88cb0efd689
Stephanie <stephanie.salazar.12@sansano.usm.cl>
2019-10-10 17:03:02 -0300

    11

--- database/migrations/1570736315940_calendar_schema.js
+++ database/migrations/1570736315940_calendar_schema.js
@@ -7,10 +7,10 @@ class CalendarSchema extends Schema {
   up () {
     this.create('calendars', (table) => {
       table.increments();
-      //table.integer('doctor_id').unsigned().references('doctor_id').inTable('doctors').onDelete('CASCADE');
-      //table.date('date');
-      //table.time('hour');
-      //table.boolean('available_hour');
+      table.integer('doctor_id').unsigned().references('doctor_id').inTable('doctors').onDelete('CASCADE');
+      table.date('date');
+      table.time('hour');
+      table.boolean('available_hour');
       table.timestamps();
     })
   }
