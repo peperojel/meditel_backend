f3586ad7caf32abc1638d9983ad645526a5ea84b
peperojel <joserojel9@gmail.com>
2019-10-10 20:52:13 -0300

    [Fix] Problema con BBDD

--- app/Controllers/Http/CalendarController.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-class CalendarController {
-}
-
-module.exports = CalendarController
--- app/Models/Calendar.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
-
-class Calendar extends Model {
-}
-
-module.exports = Calendar
