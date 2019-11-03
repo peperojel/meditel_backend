d950e133ce45d9a045e206cfccc096d97716d277
peperojel <joserojel9@gmail.com>
2019-11-03 16:00:48 -0300

    Update Procfile & SocketController

--- app/Controllers/Ws/SocketController.js
+++ app/Controllers/Ws/SocketController.js
@@ -11,6 +11,46 @@ class SocketController {
     this.storeConnection(this.sc)
   }
 
+  onMessage (message) {
+    const {type, data} = message
+
+    switch (type) {
+      case 'asesoria:request':
+        //TODO: DEBE SER CAMBIADO A BROADCAST
+        this.socket.broadcastToAll('message', {
+          type: 'asesoria:request',
+          data: {
+            nombre: this.auth.user.nombre,
+            apellido: this.auth.user.apellido
+          }
+        });
+        break;
+      case 'asesoria:start':
+        this.socket.broadcastToAll('message', {
+          type: 'asesoria:start',
+          data: 'Wena'
+        });
+        break;
+      case 'asesoria:ready':
+        this.updateState(this.sc,this.socket.topic)
+      break;
+      case 'asesoria:signaling':
+        this.socket.broadcast('message', {
+          type: 'asesoria:signaling',
+          data: data
+        });
+        break;
+      default:
+        console.log("Default case")
+      break;
+    }
+  }
+
+  onClose () {
+    this.deleteConnection(this.sc)
+  }
+
+  // Métodos de uso interno
   async storeConnection (sc) {
     sc.topic = this.socket.topic
     sc.socket_id = this.socket.id
@@ -19,39 +59,40 @@ class SocketController {
     await sc.save()
   }
 
-  onMessage (message) {
-    const {type, data} = message
-
-    switch (type) {
-      case 'asesoria:request':
-        this.socket.broadcast('message', {
-          type: 'asesoria:request',
-          data: {
-            nombre: this.auth.use.nombre,
-            apellido: this.auth.user.apellido
-          }
-        });
-        break;
-      case 'asesoria:start':
-        this.socket.broadcastToAll('message', {
-          type: 'asesoria:start',
-          data: ''
-        });
-        break;
-      case 'asesoria:ready':
-        this.sc.is_ready = true;
-        await this.sc.save();
-        const part = await SocketConnection
+  async updateState (sc, topic) {
+    sc.is_ready = true
+    await sc.save()
+    const part = await SocketConnection
           .query()
           .where({
-            topic: this.topic,
-            is_ready: true
-          })
+            topic: topic,
+            is_ready: true})
           .fetch()
-          // Contar elementos de part
+    console.log(part.toJSON().length)
+    if(part.toJSON().length === 2) {
+      this.sendReady();
+    } else {
+      this.sendInit();
     }
+  }
 
+  async deleteConnection (sc) {
+    await sc.delete();
+  }
 
+  sendReady = () => {
+    this.socket.broadcastToAll('message', {
+      type: 'asesoria:ready',
+      data: ''
+    });
+  }
+
+  //TODO: Eliminar esta función de testing
+  sendInit = () => {
+    this.socket.emit('message', {
+      type: 'asesoria:initiator',
+      data: ''
+    })
   }
 }
 
