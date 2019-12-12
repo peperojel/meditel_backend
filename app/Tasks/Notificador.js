6ae398b2acd79ca9ccbd4acb5bb0ea21465e2371
peperojel <joserojel9@gmail.com>
2019-12-11 21:20:29 -0300

    To-do Notificador

--- app/Tasks/Notificador.js
+++ app/Tasks/Notificador.js
@@ -3,12 +3,30 @@
 const moment = require('moment');
 const Task = use('Task');
 const firebase = use('FirebaseAdmin');
+// Sumar Mail
 
 class Notificador extends Task {
   static get schedule () {
     return '* * * * *'
   }
 
+  // Query que trae las asesorías en estado futuro y que no han sido notificadas.
+  // Donde la condición de "por comenzar" se obtendrá a través de el campo
+  // fecha, el cual será un entero. Para obtener la fecha de referencia se obtiene la fecha actual y se le suman 10 minutos
+  // Devuelve results
+
+  // Sacar id_paciente e id_medico para cada elemento de result
+  // Enviar correo a id_medico (User y saco su correo)
+    // Contenido:
+    //  Lo anotó Stephi
+  // Enviar notificación push a id_paciente (a través de User->FirebaseToken)
+
+  // A results se le cambian las siguientes entradas:
+  //  notificada: true
+  //  estado: "en espera"
+
+  // Anexo: Sacar la fecha actual 
+  // moment().addMinutes(10).unix()
   async handle () {
     var registrationToken = 'd5plqQwal6M:APA91bG__3EgMQvKwn7IAAj9i-bfbiRAV201OY9PZrzeYLeW5aRTi0uvojs8hOC1euqj9ao4pAk7wrQaEMCd4cVFNcYY4lXYJ6p5vmlHrIo-RK12MxKjlE1zapZ825uNcj-g8a_d79oi';
 
